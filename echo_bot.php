a7164b2e1871eb42884c94aadeb200b45cb985b5
orenzlover <orenzlover01@gmail.com>
2017-11-08 17:42:36 +0900

    initial

---
+++ echo_bot.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+require_once('./LINEBotTiny.php');
+$channelAccessToken = 'seutQQufbW5eDjujuOW3G8YE3dKBaTMDiZwZvR8BL+DHqBjDuXVJ3RTbMlZLrNL1dB/Ep4yoUwDb2fLgMzfqWCInsEs0W84S2l3X7nBUANlW66HigphHI3OQmq0AgvjDzbt5/4BuVdE0Z27gTSvX5AdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '57890b45e15601974e286cde00d647e7';
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text']
+                            )
+                        )
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
